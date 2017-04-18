f6625f1a89e6fb5ddafa7f6634897498d1bf98b1
spyjaro <spyjaro@gmail.com>
2017-04-19 01:36:10 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var tasks = [];
+//add elements into list
+function addList() {
+    var todolist = document.getElementById('todolist');
+    var task = document.getElementById('add-task__input');
+    var newLi = document.createElement('li');
+    var checkBoxLi= document.createElement('input');
+    checkBoxLi.setAttribute('class', 'input__Checkbox')
+    var span = document.createElement('span');
+    var cross = '&times;';
+    checkBoxLi.type = 'checkbox';
+    span.setAttribute('class','deleteButton');
+    if(!task.value){return false}
+
+    var taskText = document.createTextNode(task.value);
+    todolist.appendChild(newLi).appendChild(checkBoxLi);
+    newLi.appendChild(taskText);
+    newLi.appendChild(span).innerHTML= '&times;';
+    tasks.push(taskText);
+    task.value= task.defaultValue;
+}
+
+//test for press Enter
+var inputKeyCode = document.querySelector('input').addEventListener('keydown' ,testForKeyCode);
+function testForKeyCode(e) {
+    if (e.keyCode == 13) {
+        addList();
+    }
+}
+//delete function for X
+var deleteButtons = document.getElementById('todolist').addEventListener('click', deleteTasks);
+function deleteTasks(event) {
+    if (event.target.classList.contains('deleteButton')) {
+        event.target.parentNode.style.display = 'none';
+    }
+    else if(event.target.checked ) {
+        event.target.parentNode.style.textDecoration = "line-through";
+        event.target.parentNode.style.background = 'lightgrey';
+    }
+    else if(!event.target.checked) {
+        event.target.parentNode.style.textDecoration = "none";
+        event.target.parentNode.style.background = '#F2F1F2';
+    }
+}
+
+
+
